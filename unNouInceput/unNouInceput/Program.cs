dab465c2e479b825de92dc37b12c1857a077828a
Mihai Alex <alexmsc123@gmail.com>
2019-10-15 18:51:53 +0100

    Am adaugat un nou exemplu

---
+++ unNouInceput/unNouInceput/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace unNouInceput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("Hello World!");
+            p.IcanSpeakSoWell("Muie PSD");
+            Console.ReadLine();
+        }
+
+        public void IcanSpeakSoWell(string asd)
+        {
+            Console.WriteLine("Someone told me to say: " + asd);
+        }
+    }
+}
